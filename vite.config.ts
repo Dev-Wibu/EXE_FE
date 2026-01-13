be629eeb6d07150114dfa856273a37b09c2ab8a4
Dev Wibu <tantnse184055@fpt.edu.vn>
2026-01-14 06:48:57 +0700

    refactor: reorder imports in vite.config.ts for clarity

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-/// <reference types="vitest/config" />
 import path from "path";
+import { defineConfig } from "vitest/config";
 
 // https://vite.dev/config/
 export default defineConfig({
